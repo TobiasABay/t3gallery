e04bdfe52ab855a7e58c32bd440b1688a80c2f5f
Tobias Bay <tobiasabay@gmail.com>
2025-04-04 10:18:37 +0200

    new changes

---
+++ src/components/navbar.tsx
@@ -0,0 +1,8 @@
+export default function TopNav() {
+    return(
+      <nav className="flex item-center justify-between p-4 ">
+        <div className="font-size: 3px font-bold ">Gallery</div>
+        <div>Login</div>
+      </nav>
+    )
+  }
