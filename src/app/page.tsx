e7b9f096fe1e8b2cfeff2720b23298312cb9ff8e
Tobias Bay <tobiasabay@gmail.com>
2025-04-04 10:12:01 +0200

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import TopNav from "t3g/components/navbar";
+
+const mockUrls = [
+  "https://5bx9uoedh0.ufs.sh/f/Mza1PlXrJeZs0sXzUK6IYLeW1SfqOpcgQ9CFiyxMvKmk7ZAE",
+  "https://5bx9uoedh0.ufs.sh/f/Mza1PlXrJeZsJUePxJQWRlba8ZHF05oO6Q9mSxyBqMYzvhrU",
+  "https://5bx9uoedh0.ufs.sh/f/Mza1PlXrJeZsg3aoqNpwsX1cAquLn2Opdk0lPWCN3ZVb6yYa",
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      
+    <main className="min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <TopNav />
+      <div className="flex flex-wrap">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-1/4 p-4">
+          <img src={image.url} alt="image" />
+          </div>
+        ))}
+        </div>
     </main>
   );
 }
